b15c2cd30059c1e4221de576d1fbb3cc6fb62789
QZzcoder <931648183@qq.com>
2020-03-27 22:58:43 -0700

    c/s 一对一通信

--- client.cpp
+++ client.cpp
@@ -1,40 +1,77 @@
 #include<stdio.h>
 #include<sys/types.h>
 #include<stdlib.h>
-#include<string>
+#include<string.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<iostream>
+#include<cstring>
+
 
 using namespace std;
-
-int main()
-{
+int getClientSocket(int port,string ip){
     int socket_fd = socket(AF_INET, SOCK_STREAM,0);
     if(socket_fd == -1)
     {
         cout<<"socket 创建失败："<<endl;
         exit(-1);
     }
-
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(8887);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip.c_str());
     bool nOptval = true;
     setsockopt(socket_fd,SOL_SOCKET,SO_REUSEADDR,(const void*)&nOptval,sizeof(bool));
-    
     int res = connect(socket_fd,(struct sockaddr*)&addr,sizeof(addr));
-    if(res == -1)
-    {
+    if(res == -1){
         cout<<"bind 链接失败："<<endl;
         exit(-1);
     }
-    cout<<"bind 链接成功："<<endl;
+    return socket_fd;
+}
+bool mySelect(fd_set& fds,int fd,int usec){
+    timeval timeout = {0};
+    timeout.tv_usec = usec;
+    FD_ZERO(&fds);
+    FD_SET(fd,&fds);
+    switch(select(fd+1,&fds,NULL,NULL,&timeout)){
+        case -1:{
+            exit(1);
+        }case 0:break;
+        default:{
+            if(FD_ISSET(fd,&fds)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int main()
+{
+    int socket_fd = getClientSocket(8887,"127.0.0.1");
 
     write(socket_fd,"hello hebinbing",15);
+    fd_set fds;
+    timeval timeout = {0};
+    timeout.tv_usec = 500;
+    char recv_buff[255];
+    while(1){
+        if(mySelect(fds,socket_fd,500)){
+            ssize_t recv_size = read(socket_fd,recv_buff,255);
+            printf("%s\n",recv_buff);
+            memset(recv_buff,0,sizeof(recv_buff));
+        }
+        if(mySelect(fds,STDIN_FILENO,500)){
+            ssize_t recv_size = read(STDIN_FILENO,recv_buff,255);
+            if(send(socket_fd,recv_buff,sizeof(recv_buff),0)<=0){
+                cout<<"write error\n";
+                exit(1);
+            }
+            memset(recv_buff,0,sizeof(recv_buff));
+        }
+    }
 
     close(socket_fd);
 
--- server.cpp
+++ server.cpp
@@ -9,24 +9,17 @@
 
 using namespace std;
 
-int main()
-{
-    //1.创建一个socket
+int getServerSocket(int port,string ip){
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_fd == -1)
     {
         cout << "socket 创建失败： "<< endl;
         exit(1);
     }
-    //2.准备通讯地址（必须是服务器的）192.168.1.49是本机的IP
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(8887);//将一个无符号短整型的主机数值转换为网络字节顺序，即大尾顺序(big-endian)
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");//net_addr方法可以转化字符串，主要用来将一个十进制的数转化为二进制的数，用途多于ipv4的IP转化。
-    //3.bind()绑定
-    //参数一：0的返回值（socket_fd）
-    //参数二：(struct sockaddr*)&addr 前面结构体，即地址
-    //参数三: addr结构体的长度
+    addr.sin_port = htons(port);//将一个无符号短整型的主机数值转换为网络字节顺序，即大尾顺序(big-endian)
+    addr.sin_addr.s_addr = inet_addr(ip.c_str());//net_addr方法可以转化字符串，主要用来将一个十进制的数转化为二进制的数，用途多于ipv4的IP转化。
     bool nOptval = true;
     setsockopt(socket_fd,SOL_SOCKET,SO_REUSEADDR,(const void*)&nOptval,sizeof(bool));
     int res = bind(socket_fd,(struct sockaddr*)&addr,sizeof(addr));
@@ -35,6 +28,28 @@ int main()
         cout << "bind创建失败： " << endl;
         exit(-1);
     }
+    return socket_fd;
+}
+bool mySelect(fd_set& fds,int fd,int usec){
+    timeval timeout = {0};
+    timeout.tv_usec = usec;
+    FD_ZERO(&fds);
+    FD_SET(fd,&fds);
+    switch(select(fd+1,&fds,NULL,NULL,&timeout)){
+        case -1:{
+            exit(1);
+        }case 0:break;
+        default:{
+            if(FD_ISSET(fd,&fds)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int main()
+{
+    int socket_fd = getServerSocket(8887,"127.0.0.1");
     cout << "bind ok 等待客户端的连接" << endl;
     //4.监听客户端listen()函数
     //参数二：进程上限，一般小于30
@@ -51,18 +66,28 @@ int main()
     //6.使用第5步返回socket描述符，进行读写通信。
     char *ip = inet_ntoa(client.sin_addr);
     cout << "客户： 【" << ip << "】连接成功" << endl;
-  
+
     write(fd, "welcome", 7);
-   
-    char buffer[255]={};
-    int size = read(fd, buffer, sizeof(buffer));//通过fd与客户端联系在一起,返回接收到的字节数
-    //第一个参数：accept 返回的文件描述符
-    //第二个参数：存放读取的内容
-    //第三个参数：内容的大小
+    fd_set fds;
+    timeval timeout = {0};
+    timeout.tv_usec = 500;
+    char recv_buff[255];
+    while(1){
+        if(mySelect(fds,fd,500)){
+            ssize_t recv_size = read(fd,recv_buff,255);
+            printf("%s\n",recv_buff);
+            memset(recv_buff,0,sizeof(recv_buff));
+        }
 
-    cout << "接收到字节数为： " << size << endl;
-    cout << "内容： " << buffer << endl;
-
+        if(mySelect(fds,STDIN_FILENO,500)){
+            ssize_t recv_size = read(STDIN_FILENO,recv_buff,255);
+            if(send(fd,recv_buff,sizeof(recv_buff),0)<=0){
+                cout<<"write error\n";
+                exit(1);
+            }
+            memset(recv_buff,0,sizeof(recv_buff));
+        }
+    }
     //7.关闭sockfd
     close(fd);
     close(socket_fd);
